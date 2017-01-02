13879e4434f0469c4f61119dadc873269f6cf3be
tienbm92 <tienbm92@gmail.com>
2017-01-03 00:40:57 +0700

    Add IImage, View screen

---
+++ TuViNo1/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  TuViNo1
+//
+//  Created by Bùi Minh Tiến on 1/3/17.
+//  Copyright © 2017 TienBM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
