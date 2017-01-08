3324ba23907a14b1f211db5b53eb8b73d39ec0a3
tienbm92 <tienbm92@gmail.com>
2017-01-08 11:26:26 +0700

    create HomeView and cell for this screen

---
+++ TuViNo1/ItemHomeCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemHomeCollectionViewCell.swift
+//  TuViNo1
+//
+//  Created by Bùi Minh Tiến on 1/6/17.
+//  Copyright © 2017 TienBM. All rights reserved.
+//
+
+import UIKit
+
+class ItemHomeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    @IBOutlet weak var ImageCell: UIImageView!
+
+}
